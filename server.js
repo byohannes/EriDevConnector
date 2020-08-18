1ae97a9f5aa591c165e9935195f00387b50558f9
byohannes <byohannes44@gmail.com>
2020-08-18 19:33:22 +0100

    added passport for user authentication by using jwt token

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const passport = require("passport");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
@@ -9,7 +9,7 @@ const posts = require("./routes/api/posts");
 const app = express();
 
 //body parser middleware
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //DB config
@@ -24,9 +24,11 @@ mongoose
   })
   .then(() => console.log("mongoDB connected ..."))
   .catch((err) => console.log(err));
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+// passport middleware
+app.use(passport.initialize());
+
+// passport config
+require("./config/passport.js")(passport);
 
 //use Routes
 
@@ -34,6 +36,6 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
