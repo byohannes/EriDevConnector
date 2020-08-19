3362a27a4bad14a27f41cde85007267d1c924da8
byohannes <byohannes44@gmail.com>
2020-08-19 18:32:12 +0100

    created profile route with token authentication

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 router.get("/test", (req, res) => {
   res.status(200).json({
@@ -7,4 +12,21 @@ router.get("/test", (req, res) => {
   });
 });
 
-module.exports=router;
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then((profile) => {
+        if (!profile) {
+          errors.noProfile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch((err) => res.status(404).json(err));
+  }
+);
+
+module.exports = router;
