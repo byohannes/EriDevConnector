b0a1decf03c807b93c83c2018cfe1e39d6f4575e
byohannes <byohannes44@gmail.com>
2020-08-19 19:13:52 +0100

    updated profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,11 +6,7 @@ const passport = require("passport");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 
-router.get("/test", (req, res) => {
-  res.status(200).json({
-    msg: "Profile works",
-  });
-});
+const validateProfileInput = require("../../validation/profile");
 
 router.get(
   "/",
@@ -26,6 +22,71 @@ router.get(
         res.json(profile);
       })
       .catch((err) => res.status(404).json(err));
+  }
+);
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+    // check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    // Get all fields from the form
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (req.body.handle) profileFields.handle = req.body.handle;
+    if (req.body.website) profileFields.website = req.body.website;
+    if (req.body.company) profileFields.company = req.body.company;
+    if (req.body.location) profileFields.location = req.body.location;
+    if (req.body.bio) profileFields.bio = req.body.bio;
+    if (req.body.status) profileFields.status = req.body.status;
+    if (req.body.githubUsername)
+      profileFields.githubUsername = req.body.githubUsername;
+
+    // Skills - split into array
+
+    if (typeof req.body.skills !== "undefined") {
+      profileFields.skills = req.body.skills.split(",");
+    }
+    // Social
+    profileFields.social = {};
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
+      .then((profile) => {
+        if (profile) {
+          //update
+          Profile.findOneAndUpdate(
+            { user: req.user.id },
+            { $set: profileFields },
+            { new: true }
+          ).then((profile) => res.json(profile));
+        } else {
+          //create
+
+          // Check  if handle exists
+          Profile.findOne({ handle: profileFields.handle }).then((profile) => {
+            if (profile) {
+              errors.handle = "That handle already exists";
+              res.status(400).json(errors);
+            }
+
+            // Save profile
+            new Profile(profileFields)
+              .save()
+              .then((profile) => res.json(profile));
+          });
+        }
+      });
   }
 );
 
