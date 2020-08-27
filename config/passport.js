6b438917962496ab096a78d5b06e56caebc06ff4
byohannes <byohannes44@gmail.com>
2020-08-28 00:33:01 +0100

    created new key

--- config/passport.js
+++ config/passport.js
@@ -1,15 +1,12 @@
-const passport = require("passport");
-
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
-
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
 const keys = require("../config/keys");
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = K = keys.secretOrKey;
+opts.secretOrKey = keys.secretOrKey;
 
 module.exports = (passport) => {
   passport.use(
