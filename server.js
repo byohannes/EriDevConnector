9752b52f050f4ae35827344b12ece9e1c0f8e53b
byohannes <byohannes44@gmail.com>
2020-08-28 00:32:33 +0100

    updated server with new routes

--- server.js
+++ server.js
@@ -2,36 +2,33 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
 
-//body parser middleware
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//DB config
+// DB Config
 const db = require("./config/keys").mongoURI;
 
-//connect to mongodb
+// Connect to MongoDB
 mongoose
-  .connect(db, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("mongoDB connected ..."))
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
-// passport middleware
+
+// Passport middleware
 app.use(passport.initialize());
 
-// passport config
-require("./config/passport.js")(passport);
+// Passport Config
+require("./config/passport")(passport);
 
-//use Routes
-
+// Use Routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
