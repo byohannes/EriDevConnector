810a2836ac6365e322b14d7502286038194176c2
byohannes <byohannes44@gmail.com>
2020-10-31 18:27:48 +0000

    updated navbar links of sign in and sign up

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
       <div className="container">
-        <a className="navbar-brand" href="landing.html">
+        <Link className="navbar-brand" to="/">
           DevConnector
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -27,14 +28,14 @@ const Navbar = () => {
 
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <a className="nav-link" href="register.html">
+              <Link className="nav-link" to="/register">
                 Sign Up
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="login.html">
+              <Link className="nav-link" to="/login">
                 Login
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
