0954f4aefeeda5fde9b9f2d6e7b39feba0c70f1b
byohannes <byohannes44@gmail.com>
2020-10-31 18:25:21 +0000

    updated mani APP file to include router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 
 const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <div className="container">
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
