aebb88bbefacd22c16f5322ad59f1da8aebdbd8e
byohannes <byohannes44@gmail.com>
2020-10-31 18:27:11 +0000

    updated home page login and sign up button to link into login and register pages

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -13,12 +14,12 @@ const Landing = () => {
                 from other developers
               </p>
               <hr />
-              <a href="register.html" className="btn btn-lg btn-info mr-2">
+              <Link to="/register" className="btn btn-lg btn-info mr-2">
                 Sign Up
-              </a>
-              <a href="login.html" className="btn btn-lg btn-light">
+              </Link>
+              <Link to="/login" className="btn btn-lg btn-light">
                 Login
-              </a>
+              </Link>
             </div>
           </div>
         </div>
