e06f88462a7b066e59f8ea7061e16a7f27d547aa
byohannes <byohannes44@gmail.com>
2020-11-09 23:26:48 +0000

    updated APP component to initialize redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Navbar from "./components/layout/Navbar";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+import "./App.css";
+
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <div className="container">
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </Router>
+      </Router>
+    </Provider>
   );
 };
 
