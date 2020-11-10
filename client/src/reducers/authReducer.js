d0a5e6433ba2831215b287181cbc7e05e7a0152d
byohannes <byohannes44@gmail.com>
2020-11-10 22:30:25 +0000

    created redux reducers

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,18 @@
+import { TEST_DISPATCH } from "./../actions/types";
+
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+};
+
+export default function auth(state = initialState, action) {
+  switch (action.type) {
+    case TEST_DISPATCH:
+      return {
+        ...state,
+        user: action.payload,
+      };
+    default:
+      return state;
+  }
+}
