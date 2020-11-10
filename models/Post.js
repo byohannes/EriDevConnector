68b6c5bd09839f69c157ab61cbc2f16a5a29ecf4
byohannes <byohannes44@gmail.com>
2020-11-10 22:39:53 +0000

    updated post model

--- models/Post.js
+++ models/Post.js
@@ -1,56 +1,52 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Create Schema
 const PostSchema = new Schema({
   user: {
-    type: Schema.Types.ObjectId,
-    ref: "users",
+    type: Schema.Types.ObjectId
   },
   text: {
     type: String,
-    required: true,
+    required: true
   },
   name: {
-    type: String,
+    type: String
   },
   avatar: {
-    type: String,
+    type: String
   },
   likes: [
     {
       user: {
-        type: Schema.Types.ObjectId,
-        ref: "users",
-      },
-    },
+        type: Schema.Types.ObjectId
+      }
+    }
   ],
   comments: [
     {
       user: {
-        type: Schema.Types.ObjectId,
-        ref: "users",
+        type: Schema.Types.ObjectId
       },
       text: {
         type: String,
-        required: true,
+        required: true
       },
       name: {
-        type: String,
+        type: String
       },
       avatar: {
-        type: String,
+        type: String
       },
       date: {
         type: Date,
-        default: Date.now,
-      },
-    },
+        default: Date.now
+      }
+    }
   ],
   date: {
     type: Date,
-    default: Date.now,
-  },
+    default: Date.now
+  }
 });
 
-module.exports = Post = mongoose.model("post", PostSchema);
+module.exports = mongoose.model('post', PostSchema);
